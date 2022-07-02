acbf8fdb7241db73caa30ea92e01d0b8f0bfdf4c
Felix <felixd1912@gmail.com>
2022-07-02 22:22:38 +0200

    kurzer zwischenstand

--- app/src/main/java/com/example/fahrtenbuch/ui/settings/SelectGpsFragment.java
+++ app/src/main/java/com/example/fahrtenbuch/ui/settings/SelectGpsFragment.java
@@ -2,6 +2,7 @@ package com.example.fahrtenbuch.ui.settings;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -14,10 +15,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.fahrtenbuch.R;
 import com.example.fahrtenbuch.databinding.FragmentSelectGpsBinding;
+import com.example.fahrtenbuch.db.Database;
 
 import java.util.HashSet;
 import java.util.function.Consumer;
@@ -40,7 +44,7 @@ public class SelectGpsFragment extends Fragment implements View.OnClickListener{
         binding.cardZuhause.setOnClickListener(this);
         binding.cardUni.setOnClickListener(this);
         binding.cardArbeit.setOnClickListener(this);
-        binding.cardFreunde.setOnClickListener(this);
+        binding.cardEinkauf.setOnClickListener(this);
         binding.cardSonstiges.setOnClickListener(this);
 
         View root = binding.getRoot();
@@ -49,29 +53,28 @@ public class SelectGpsFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
+        FragmentTransaction fragmentTransaction= getParentFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_fragment_container, new MapFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        requireActivity().getSupportFragmentManager().setFragmentResultListener("requestKey", getViewLifecycleOwner(), new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                Database db = new Database(binding.getRoot().getContext());
+                db.insertLocation();
+            }
+        });
+
+
         if (view == binding.cardZuhause) {
-            FragmentTransaction fragmentTransaction= getParentFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.main_fragment_container, new LocationPickerFragment());
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-            orteZuhause.add(new Location(SettingsFragment.lastEndpointBluetoothBeacon));
-            orteZuhause.add(new Location(SettingsFragment.lastEndpointBluetoothBeacon));
         }
-        else if (view == binding.cardFreunde) {
-            orteFreunde.add(new Location(SettingsFragment.lastEndpointBluetoothBeacon));
+        else if (view == binding.cardEinkauf) {
         }
         else if (view == binding.cardUni) {
-            FragmentTransaction fragmentTransaction= getParentFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.main_fragment_container, new MapFragment());
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-            orteUni.add(new Location(SettingsFragment.lastEndpointBluetoothBeacon));
         }
         else if (view == binding.cardArbeit) {
-            orteArbeit.add(new Location(SettingsFragment.lastEndpointBluetoothBeacon));
         }
         else if (view == binding.cardSonstiges) {
-            orteSonstiges.add(new Location(SettingsFragment.lastEndpointBluetoothBeacon));
         }
     }
 }
