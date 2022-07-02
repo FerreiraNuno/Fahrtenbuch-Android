bb7f08f2ef09e72958c02846615393b87df764ae
Martin <martin.holstein@mni.thm.de>
2022-07-02 22:33:18 +0200

    6.1. Commit: Push Nachrichten nun ohne schrillen sound.

--- app/src/main/java/com/example/fahrtenbuch/ui/settings/PushNotificationHandler.java
+++ app/src/main/java/com/example/fahrtenbuch/ui/settings/PushNotificationHandler.java
@@ -24,10 +24,12 @@ public class PushNotificationHandler {
        public PushNotificationHandler(Context context){
            this.myContext = context;
            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-               NotificationChannel channel = new NotificationChannel("myChannel", "PushNotifier" , NotificationManager.IMPORTANCE_DEFAULT);
+               NotificationChannel channel = new NotificationChannel("myChannel2", "PushNotifier" , NotificationManager.IMPORTANCE_DEFAULT);
 
                NotificationManager manager = myContext.getSystemService(NotificationManager.class);
+               channel.setSound(null, null);
                manager.createNotificationChannel(channel);
+
 
 
 
@@ -36,18 +38,21 @@ public class PushNotificationHandler {
 
        public void pushNotifcation(String Start, String Ziel, int strecke, int typ){
 
-          /* Intent resultInt = new Intent(myContext, CreateRideFragment.class);
-           TaskStackBuilder stackBuilder = TaskStackBuilder.create(myContext);
+          Intent resultInt = new Intent(myContext, CreateRideFragment.class);
+           resultInt.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+          resultInt.putExtra("pushRide", "CreateRideFragment");
+           PendingIntent resultPendInt = PendingIntent.getService(myContext, 0, resultInt, 0);
+          /* TaskStackBuilder stackBuilder = TaskStackBuilder.create(myContext);
            stackBuilder.addNextIntentWithParentStack(resultInt);
            PendingIntent resultPendInt =
                    stackBuilder.getPendingIntent(0,
                            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);*/
 
-           NotificationCompat.Builder  builder = new NotificationCompat.Builder(myContext, "myChannel")
+           NotificationCompat.Builder  builder = new NotificationCompat.Builder(myContext, "myChannel2")
                    .setSmallIcon(android.R.drawable.star_big_on) //Das Icon der Notifikation
                    .setContentTitle("Eine Neue Fahrt!")
                    .setContentText("Ihre Fahrt ging von "+ Start + " bis " + Ziel + " und ging " + strecke + " km :D")
-                   //.setContentIntent(resultPendInt)
+                   .setContentIntent(resultPendInt)
                    ;
 
            notification = builder.build();
